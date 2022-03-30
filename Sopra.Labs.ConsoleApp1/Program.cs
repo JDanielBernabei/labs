2d2aa02c61c93dff0427e5a16fe23a2b21bc62a8
JDanielBernabei <jose-daniel.gonzalez@soprasteria.com>
2022-03-30 09:58:52 +0200

    Correcciones de casos límite

--- Sopra.Labs.ConsoleApp1/Program.cs
+++ Sopra.Labs.ConsoleApp1/Program.cs
@@ -41,14 +41,15 @@ namespace Sopra.Labs.ConsoleApp1
             char[] letras = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
 
             Console.WriteLine("Introduzca DNI (sin letra)");
-            bool result = int.TryParse(Console.ReadLine(), out numeroDni);
+            string linea = Console.ReadLine();
+            bool result = int.TryParse(linea, out numeroDni);
 
-            if (result)
-            {
-                Console.WriteLine($"La letra del DNI es: {letras[numeroDni % 23]}");
-            } else
+            if (!result || numeroDni<0 || linea.Length != 8)
             {
                 Console.WriteLine("El número introducido no es correcto");
+            } else
+            {
+                Console.WriteLine($"La letra del DNI es: {letras[numeroDni % 23]}");
             }
     }
 
@@ -58,8 +59,14 @@ namespace Sopra.Labs.ConsoleApp1
             Console.WriteLine("Introduzca un numero");
             bool result = int.TryParse(Console.ReadLine(), out num);
 
+            if (!result)
+            {
+                Console.WriteLine("No se ha introducido un número");
+                return;
+            }
+
             //Utilizando un For
-            Console.WriteLine($"(Bucle for) - Tabla de multiplicar del {num}:");
+                Console.WriteLine($"(Bucle for) - Tabla de multiplicar del {num}:");
             for (int i= 0; i < 11; i++)
             {
                 Console.WriteLine($"{num} x {i} = {num * i}");
@@ -89,6 +96,12 @@ namespace Sopra.Labs.ConsoleApp1
             Console.WriteLine("Introduzca el salto");
             result = int.TryParse(Console.ReadLine(), out salto);
 
+            if ((inicio > final && salto > 0) || (inicio < final && salto < 0))
+            {
+                Console.WriteLine("El salto tiene el signo erroneo");
+                return;
+            }
+
             for (int i = inicio; i < final; i += salto) 
             {
                 Console.WriteLine($"{i}");
@@ -114,17 +127,15 @@ namespace Sopra.Labs.ConsoleApp1
             }
 
             int[] array = new int[size];
-            int max = Int32.MaxValue, min = Int32.MinValue, mean, summ = 0;
+            int max = Int32.MinValue;
+            int min = Int32.MaxValue;
+            float mean;
+            int summ = 0;
 
             Console.WriteLine("Introduzca valores");
             for (int i = 0; i < size; i++)
             {
                 result = int.TryParse(Console.ReadLine(), out array[i]);
-                if (i == 0)
-                {
-                    max = array[i];
-                    min = array[i];
-                }
                 if (max < array[i]) max = array[i];
                 if (min > array[i]) min = array[i];
                 summ += array[i];
