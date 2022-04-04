e99423b49d027fa579a5b5641c1341a2fe7d92b7
JDanielBernabei <jose-daniel.gonzalez@soprasteria.com>
2022-04-04 17:30:37 +0200

    Ejercicio Estudiante

--- Sopra.Labs.ConsoleApp4/Program.cs
+++ Sopra.Labs.ConsoleApp4/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
+using System.Text;
 using Newtonsoft.Json;
+using System.Net.Http.Formatting;
+using System.Net.Http.Json;
 
 namespace Sopra.Labs.ConsoleApp4
 {
@@ -9,6 +14,37 @@ namespace Sopra.Labs.ConsoleApp4
         static void Main(string[] args)
         {
             ZipInfo();
+        }
+
+        static void Estudiante()
+        {
+            // Obtener los datos del estudiante 11. Dos metodos, extenso y abreviado
+            //http://school.labs.com.es/api/students/11
+
+            Console.Write("Estudiante: ");
+
+            HttpResponseMessage response = http.GetAsync("http://school.labs.com.es/api/students/11").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var data = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
+                Console.WriteLine($"ID: {data["id"]}");
+                Console.WriteLine($"Nombre: {data["firstName"]} {data["lastName"]}");
+                Console.WriteLine($"Fecha de nacimiento: {data["dateOfBirth"]}");
+                Console.WriteLine($"Clase ID: {data["classId"]}, Clase: {data["class"]}");
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+
+            Console.WriteLine();
+
+            // Metodo 2
+            var data2 = http.GetFromJsonAsync<Student>("http://school.labs.com.es/api/students/11").Result;
+            Console.WriteLine($"ID: {data2.Id}");
+            Console.WriteLine($"Nombre: {data2.Firstname} {data2.Lastname}");
+            Console.WriteLine($"Fecha de nacimiento: {data2.DateOfBirth}");
+            Console.WriteLine($"Clase ID: {data2.ClassId}");
         }
 
         static void ZipInfo()
---
+++ Sopra.Labs.ConsoleApp4/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sopra.Labs.ConsoleApp4
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int ClassId { get; set; }
+    }
+}
